fe4d4b0c44cd785d630a77e26f51720936e407a3
DimRev <dimarev444@gmail.com>
2024-03-24 10:44:10 +0200

    Add metrics middleware to static routes

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
 func main() {
 	const port = "8080"
+	const filepathRoot = "./public"
+
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
 
@@ -15,8 +21,13 @@ func main() {
 		Handler: corsMux,
 	}
 
+	apiCfg := apiConfig{
+		fileServerHits: 0,
+	}
+
 	// * Router Handlers
-	mux.HandleFunc("/", rootHandler)
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(handlerStatic(filepathRoot)))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	log.Printf("Serving on port %s", port)
 	log.Fatal(srv.ListenAndServe())
@@ -35,6 +46,19 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./index.html")
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileServerHits++
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func handlerStatic(filepathRoot string) http.Handler {
+	return http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 }
