7cc1f87d4da7f4c87d89b1410097da8c30efb77d
DimRev <dimarev444@gmail.com>
2024-03-24 15:51:24 +0200

    Refactor, move funcs off main into their own files

--- main.go
+++ main.go
@@ -6,14 +6,25 @@ import (
 )
 
 type apiConfig struct {
-	fileServerHits int
+	fileserverHits int
 }
 
 func main() {
-	const port = "8080"
 	const filepathRoot = "./public"
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
 
 	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", fsHandler)
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
 	corsMux := middlewareCors(mux)
 
 	srv := &http.Server{
@@ -21,44 +32,6 @@ func main() {
 		Handler: corsMux,
 	}
 
-	apiCfg := apiConfig{
-		fileServerHits: 0,
-	}
-
-	// * Router Handlers
-	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(handlerStatic(filepathRoot)))
-	mux.HandleFunc("/healthz", handlerReadiness)
-
-	log.Printf("Serving on port %s", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	cfg.fileServerHits++
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func handlerStatic(filepathRoot string) http.Handler {
-	return http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 }
